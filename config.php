e50fe516c46d6b621a31a981fb4f52388532cf08
ktalpada484-cell <ktalpada484@gmail.com>
2026-07-19 18:01:44 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = [
+    'bot_token' => '8777724011:AAHAJyLN7mDafEP6F7mkaqGtW-h7VKlUdF0',
+    'chat_id'   => '8241737661',
+    'website' => 'https://sumit1123.infinityfree.io/link/',
+    'admin_username' => 'sumit',
+    'admin_password' => 'sumit1123'
+];
+?>
